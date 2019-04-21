1044efd4f1495b6640be75a526ca4f04f07ce40f
sbx_user1051 <sbx_user1051@169.254.63.185>
2019-04-21 19:37:12 +0000

    Sun Apr 21 19:37:12 UTC 2019

--- lib/solutions/CHK/checkout_solution.py
+++ lib/solutions/CHK/checkout_solution.py
@@ -3,49 +3,35 @@
 # noinspection PyUnusedLocal
 # skus = unicode string
 def checkout(skus):
-	
-	a=0
-	b=0
-	c=0
-	d=0
-	e=0
-	f=0
+	pr=[0 for i in range(skus.len())]
 	for sku in list(skus):
-		if sku=='A':
-			a+=1
-		elif sku=='B':
-			b+=1
-		elif sku=='C':
-			c+=1
-		elif sku=='D':
-			d+=1
-		elif sku=='E':
-			e+=1
-		elif sku=='F':
-			f+=1
+		if sku.isupper():
+			pr[sku]+=1;
 		else:
-			return -1
+			return -1;
+
 	
 	sum=0
 		
-	sum=sum+int(a/5)*200
-	a=int(a%5)
-	sum=sum+int(a/3)*130
-	sum=sum+int(a%3)*50
+	sum=sum+int(pr['A']/5)*200
+	pr['A']=int(pr['A']%5)
+	sum=sum+int(pr['A']/3)*130
+	sum=sum+int(pr['A']%3)*50
 	
 	
 			 
-	sum=sum+c*20
-	sum=sum+d*15
-	sum=sum+e*40
+	sum=sum+pr['C']*20
+	sum=sum+pr['D']*15
+	sum=sum+pr['E']*40
 	
-	if (b-int(e/2))>=0:
-		b=b-int(e/2)
-		sum=sum+int(b/2)*45
-		sum=sum+int(b%2)*30
+	if (pr['B']-int(pr['E']/2))>=0:
+		pr['B']=pr['B']-int(pr['E']/2)
+		sum=sum+int(pr['B']/2)*45
+		sum=sum+int(pr['B']%2)*30
 		
-	sum=sum+(f-int(f/3))*10
+	sum=sum+(pr['F']-int(pr['F']/3))*10
 	return sum
+
 
 
 
