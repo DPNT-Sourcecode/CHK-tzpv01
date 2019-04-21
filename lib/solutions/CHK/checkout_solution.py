ee352e1080877511c0389f85e769d706496a0e70
sbx_user1051 <sbx_user1051@169.254.63.185>
2019-04-21 18:51:36 +0000

    Sun Apr 21 18:51:36 UTC 2019

--- lib/solutions/CHK/checkout_solution.py
+++ lib/solutions/CHK/checkout_solution.py
@@ -3,20 +3,32 @@
 # noinspection PyUnusedLocal
 # skus = unicode string
 def checkout(skus):
-	try:
+	
+	a=0
+	b=0
+	c=0
+	d=0
+	for sku in list(skus):
+		if sku=='A':
+			a+=1
+		elif sku=='B':
+			b+=1
+		elif sku=='C':
+			c+=1
+		elif sku=='D':
+			d+=1
+		else:
+			return -1
+	
 		sum=0
-		sku=list(skus)
 		
-		sum=sum+(sku[0]/3)*130
-		sum=sum+(sku[0]%3)*50
+		sum=sum+(a/3)*130
+		sum=sum+(a%3)*50
 		
-		sum=sum+(sku[1]/2)*45
-		sum=sum+(sku[1]%2)*30
+		sum=sum+(b[1]/2)*45
+		sum=sum+(b[1]%2)*30
 			 
-		sum=sum+sku[2]*20
-		sum=sum+sku[3]*15
+		sum=sum+c*20
+		sum=sum+d*15
 		return sum
-
-	except:
-		return -1;
 
