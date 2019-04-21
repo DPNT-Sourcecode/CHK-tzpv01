7e224a3ab9606620631f111182e1b938bc3955fd
sbx_user1051 <sbx_user1051@169.254.63.185>
2019-04-21 19:03:38 +0000

    Sun Apr 21 19:03:38 UTC 2019

--- lib/solutions/CHK/checkout_solution.py
+++ lib/solutions/CHK/checkout_solution.py
@@ -8,6 +8,7 @@ def checkout(skus):
 	b=0
 	c=0
 	d=0
+	e=0
 	for sku in list(skus):
 		if sku=='A':
 			a+=1
@@ -17,20 +18,26 @@ def checkout(skus):
 			c+=1
 		elif sku=='D':
 			d+=1
+		elif sku=='E':
 		else:
 			return -1
 	
 	sum=0
 		
+	sum=sum+int(a/5)*200
+	a=int(a%5)
 	sum=sum+int(a/3)*130
 	sum=sum+int(a%3)*50
-		
+	
 	sum=sum+int(b/2)*45
 	sum=sum+int(b%2)*30
 			 
 	sum=sum+c*20
 	sum=sum+d*15
+	sum=sum+e*40
+	sum=sum+int(e%2)*30
 	return sum
+
 
 
 
